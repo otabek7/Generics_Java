53a9d275157e96f641bc35518bcc9bc56fd5e5c6
Otabek <mavlonovo@tamu.edu>
2024-01-03 22:28:15 -0600

    first commit

---
+++ generics/src/main/java/ArrayListExample.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+        ArrayList items = new ArrayList();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+
+        printDouble(items);
+
+
+    }
+
+    private static void printDouble(ArrayList items) {
+        for (Object i : items){
+            System.out.println((Integer)i*2);
+        }
+    }
+
+}
---
+++ generics/src/main/java/Team.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Team {
+
+    private String name;
+    private ArrayList<Player> members = new ArrayList<Player>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(Player player){
+        if(members.contains(player)){
+            System.out.println(player.getName() + " is already on this team.");
+            return false;
+        }
+        else{
+            members.add(player);
+            System.out.println(player.getName() + " picked for team " + this.name);
+            return true;
+        }
+    }
+
+    public int numPlayers(){
+        return this.members.size();
+    }
+
+}
